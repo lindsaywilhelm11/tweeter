79e236b39dafa2c6060e847ea6671151e763140f
Lindsay Wilhelm <lindsaywilhelm11@gmail.com>
2021-12-17 02:44:22 +0000

    fixed bug in order to display db tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,27 @@
-const tweets = [
+// const data = [
 //     {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//     ,
-//       "handle": "@SirIsaac"
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1639534560699
 //     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-// },
-// {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-// }
-];
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd"
+//       },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1639620960699
+//     }
+//   ]
 
 const escape = (str) => {
     let div = document.createElement("div");
@@ -29,23 +29,23 @@ const escape = (str) => {
     return div.innerHTML;
   };
 
-const createTweetElement = function(tweet) {
-    let $tweet = $("<article>").addClass('tweet') 
-    let tweetLayout = `
+const createTweetElement = (data) => {
+
+    let $tweet = `
     <section class="tweet-container">
     <article class="tweet">
           <header class="tweet-header">
-          <img class="tweet-img" src="${tweet.user.avatars}"> 
+          <img class="tweet-img" src="${data.user.avatars}"> 
            <div>
-          <p class="tweet-name">${tweet.user.name}</p>
+          <p class="tweet-name">${data.user.name}</p>
         </div>
              <br />
-            <p class="tweet-handle">${tweet.user.handle}</p>
+            <p class="tweet-handle">${data.user.handle}</p>
            </header>
-           <p class="tweet-message">${escape(tweet.content.text)}</p>
+           <p class="tweet-message">${escape(data.content.text)}</p>
           <hr>
           <footer class="tweet-footer">
-             <p class="timeago">${(timeago.format(tweet.created_at))}</p>
+             <p class="timeago">${(timeago.format(data.created_at))}</p>
             <div class="tweet-icons">
               <i class="fas fa-flag"></i>
               <i class="fas fa-retweet"></i>
@@ -57,19 +57,15 @@ const createTweetElement = function(tweet) {
         </section>
     `;
 
-    let tweetElement = $tweet.append(tweetLayout);
-    return tweetElement;
+    return $tweet;
   }
 
 const renderTweets = (tweets) => {
-     $('tweet-section').empty();
     for (let tweet in tweets) {
-      let $newTweet = createTweetElement(tweet);
+      let $newTweet = createTweetElement(tweets[tweet]);
       $('.tweet-section').prepend($newTweet);
     }
 }
-
-
 
 $(document).ready(function() {
     
@@ -97,23 +93,24 @@ $('.tweet-form').on('submit', (e) => {
 
     $.ajax({
         method: "POST",
-        url: 'http://localhost:8080/tweets',
+        url: '/tweets',
         data: data
       })
         .done(() => {
           loadTweets();
-          $(".tweet-text").val("");
-          $(".counter").text(140);
       })
         .fail((err) => {
           console.log(err)
+
+        $(".tweet-text").val("");
+        $(".counter").text(140);
     })
-    loadTweets(); 
   }); 
+
   const loadTweets = () => {
     $.ajax({
-      url: 'http://localhost:8080/tweets',
-      method: 'GET'
+      method: 'GET',
+      url: '/tweets'
     })
     .done((result) => {
         renderTweets(result)
@@ -122,7 +119,7 @@ $('.tweet-form').on('submit', (e) => {
         console.log(err)
     })
   }
-  loadTweets();
+  loadTweets(); 
 })
 
 
